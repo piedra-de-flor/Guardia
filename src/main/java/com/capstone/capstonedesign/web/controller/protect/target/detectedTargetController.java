6bd2ffba6daac582314d41e48a9fa730be18ba33
piedra-de-flor <xxspdlqjxx@naver.com>
2024-05-28 18:51:38 +0900

    feat : 보호 대상 추적 api에 대한 swagger 설정 추가

--- src/main/java/com/capstone/capstonedesign/web/controller/protect/target/detectedTargetController.java
+++ src/main/java/com/capstone/capstonedesign/web/controller/protect/target/detectedTargetController.java
@@ -1,6 +1,10 @@
 package com.capstone.capstonedesign.web.controller.protect.target;
 
 import com.capstone.capstonedesign.service.protect.target.DetectedService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
@@ -15,13 +19,20 @@ import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 
+@Tag(name = "보호 대상 추적 Controller", description = "Protected target detect API")
 @RequiredArgsConstructor
 @RestController
 public class detectedTargetController {
     private final DetectedService service;
 
+    @Operation(summary = "보호 대상 단일 조회", description = "자신 소유의 보호 대상을 단일 조회합니다.")
+    @ApiResponse(responseCode = "200", description = "성공")
+    @ApiResponse(responseCode = "400", description = "잘못된 요청 형식입니다")
+    @ApiResponse(responseCode = "500", description = "내부 서버 오류 발생")
     @GetMapping("/detect")
-    public ResponseEntity<byte[]> detect(@RequestParam long targetId) throws IOException {
+    public ResponseEntity<byte[]> detect(
+            @Parameter(description = "보호 대상 추적 요청값", required = true)
+            @RequestParam long targetId) throws IOException {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         String memberEmail = authentication.getName();
 
